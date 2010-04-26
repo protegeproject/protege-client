a011020b415cc299e94b857ee6add23246eaa308
Timothy Redmond <tredmond@stanford.edu>
2010-04-26 17:19:06 +0000

    initial commit

---
+++ src/org/protege/editor/owl/client/ClientOntologyBuilder.java
@@ -0,0 +1,30 @@
+package org.protege.editor.owl.client;
+
+import org.protege.editor.core.OntologyBuilder;
+import org.protege.editor.core.editorkit.EditorKit;
+
+public class ClientOntologyBuilder implements OntologyBuilder {
+
+    public ClientOntologyBuilder() {
+        // TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public boolean loadOntology(EditorKit editorKit) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public void initialise() throws Exception {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void dispose() throws Exception {
+        // TODO Auto-generated method stub
+
+    }
+
+}
---
+++ src/org/protege/editor/owl/client/ServerProjectsList.java
@@ -0,0 +1,14 @@
+package org.protege.editor.owl.client;
+
+import java.awt.BorderLayout;
+
+import javax.swing.JPanel;
+
+public class ServerProjectsList extends JPanel {
+    private static final long serialVersionUID = 1153129326606611580L;
+
+    public ServerProjectsList() {
+        setLayout(new BorderLayout());
+        
+    }
+}

c4a9790a8a9fe6e05fc7ca1f1b3e9dd6d4778fe7
Timothy Redmond <tredmond@stanford.edu>
2012-07-26 15:22:07 +0000

    stub for an update menu

---
+++ src/org/protege/editor/owl/client/UpdateAction.java
@@ -0,0 +1,27 @@
+package org.protege.editor.owl.client;
+
+import java.awt.event.ActionEvent;
+
+import org.protege.editor.core.ui.action.ProtegeAction;
+
+public class UpdateAction extends ProtegeAction {
+
+	@Override
+	public void initialise() throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void dispose() throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

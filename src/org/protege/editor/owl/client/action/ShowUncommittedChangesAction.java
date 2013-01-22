b09b02df60a4df33bdd622f7816c7ec5af537ae8
Timothy Redmond <tredmond@stanford.edu>
2013-01-22 02:20:01 +0000

    the uncommitted changes window is now resizeable

--- src/org/protege/editor/owl/client/action/ShowUncommittedChangesAction.java
+++ src/org/protege/editor/owl/client/action/ShowUncommittedChangesAction.java
@@ -1,11 +1,14 @@
 package org.protege.editor.owl.client.action;
 
+import java.awt.Frame;
 import java.awt.event.ActionEvent;
 import java.util.List;
 
+import javax.swing.JDialog;
 import javax.swing.JOptionPane;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.SwingUtilities;
 
 import org.protege.editor.core.ProtegeApplication;
 import org.protege.editor.owl.client.connect.ServerConnectionManager;
@@ -19,6 +22,7 @@ import org.protege.owl.server.util.ClientUtilities;
 import org.semanticweb.owlapi.model.OWLObject;
 import org.semanticweb.owlapi.model.OWLOntology;
 import org.semanticweb.owlapi.model.OWLOntologyChange;
+import org.semanticweb.owlapi.model.OWLOntologyID;
 
 public class ShowUncommittedChangesAction extends ProtegeOWLAction {
     private static final long serialVersionUID = -7628375950917155764L;
@@ -38,6 +42,7 @@ public class ShowUncommittedChangesAction extends ProtegeOWLAction {
         try {
             ServerConnectionManager connectionManager = ServerConnectionManager.get(getOWLEditorKit());
             OWLOntology ontology = getOWLModelManager().getActiveOntology();
+            
             VersionedOntologyDocument vont = connectionManager.getVersionedOntology(ontology);
             if (vont != null) {
                 Client client = connectionManager.createClient(ontology);
@@ -46,11 +51,7 @@ public class ShowUncommittedChangesAction extends ProtegeOWLAction {
                 if (uncommitted.isEmpty()) {
                 	JOptionPane.showMessageDialog(getOWLWorkspace(), "No uncommitted changes");
                 } else {
-                    ChangeListTableModel tableModel = new ChangeListTableModel(uncommitted);
-                    JTable table = new JTable(tableModel);
-                    table.setDefaultRenderer(OWLObject.class, new OWLCellRenderer(getOWLEditorKit()));
-                    JScrollPane pane = new JScrollPane(table);
-                    JOptionPane.showMessageDialog(getOWLWorkspace(), pane);
+                	displayUncommittedChanges(ontology, uncommitted);
                 }
             }
             else {
@@ -63,6 +64,28 @@ public class ShowUncommittedChangesAction extends ProtegeOWLAction {
         catch (Exception e) {
             ProtegeApplication.getErrorLog().logError(e);
         }
+    }
+    
+    private void displayUncommittedChanges(OWLOntology ontology, List<OWLOntologyChange> uncommitted) {
+		String shortOntologyName = "";
+		OWLOntologyID ontologyId = ontology.getOntologyID();
+		if (!ontologyId.isAnonymous()) {
+		    shortOntologyName = ontology.getOntologyID().getOntologyIRI().getFragment();
+		}
+		if (shortOntologyName.isEmpty()) {
+		    shortOntologyName = ontologyId.toString();
+		}
+        ChangeListTableModel tableModel = new ChangeListTableModel(uncommitted);
+        JTable table = new JTable(tableModel);
+        table.setDefaultRenderer(OWLObject.class, new OWLCellRenderer(getOWLEditorKit()));
+        JScrollPane pane = new JScrollPane(table);
+        JDialog dialog = new JDialog((Frame) SwingUtilities.getAncestorOfClass(Frame.class, getOWLWorkspace()));
+        dialog.setTitle("Uncommitted changes for " + shortOntologyName);
+        dialog.getContentPane().add(pane);
+        dialog.setResizable(true);
+        dialog.setModal(true);
+        dialog.pack();
+        dialog.setVisible(true);
     }
 
 }

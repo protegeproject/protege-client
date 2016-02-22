8c4fa6ef91dc8b5e8e6f6d320dfce5d2386fd549
Rafael Goncalves <goncalvjr@gmail.com>
2016-02-22 11:22:12 -0800

    Add source and javadoc maven plugins

--- src/main/java/org/protege/editor/owl/client/diff/model/OwlOntologyChangeAnnotator.java
+++ src/main/java/org/protege/editor/owl/client/diff/model/OwlOntologyChangeAnnotator.java
@@ -14,15 +14,15 @@ public interface OwlOntologyChangeAnnotator {
 
     /**
      * Get a bundle of OWL ontology changes annotated with a structured-text value based on the given parameters. These changes are subsequently
-     * interpreted as a single change (i.e., row in the diff) that bundles multiple OWLOntologyChange-level changes.
+     * interpreted as a single change (i.e., one row in the diff GUI) that bundles multiple OWLOntologyChange-level changes.
      *
      * @param changes   List of changes to be annotated
      * @param revisionTag   Revision tag
      * @param changeSubject Change subject (i.e., OWL entity)
      * @param changeType    Change type
-     * @param p Property that is
-     * @param newValue  New value
-     * @return
+     * @param p Annotation property that got changed
+     * @param newValue  New value of the (annotation) property
+     * @return List of annotated ontology changes
      */
     List<OWLOntologyChange> getAnnotatedChange(List<OWLOntologyChange> changes, RevisionTag revisionTag, OWLEntity changeSubject, ChangeType changeType,
                                                Optional<OWLEntity> p, Optional<String> newValue);

02cbc2f9cb747321d0d5e091d20b927510cb0ebf
johardi <josef.hardi@stanford.edu>
2016-03-24 17:31:17 -0700

    Code format

--- src/main/java/org/protege/editor/owl/client/action/OpenFromServerAction.java
+++ src/main/java/org/protege/editor/owl/client/action/OpenFromServerAction.java
@@ -7,20 +7,22 @@ import org.protege.editor.owl.ui.action.ProtegeOWLAction;
 
 public class OpenFromServerAction extends ProtegeOWLAction {
 
-	private static final long serialVersionUID = 1921872278936323557L;
+    private static final long serialVersionUID = 1921872278936323557L;
 
-	@Override
-	public void initialise() throws Exception {
-	}
+    @Override
+    public void initialise() throws Exception {
+        // NO-OP
+    }
 
-	@Override
-	public void dispose() throws Exception {
-	}
+    @Override
+    public void dispose() throws Exception {
+        // NO-OP
+    }
 
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		OpenFromServerDialog dialog = new OpenFromServerDialog(getOWLEditorKit());
-		dialog.setLocationRelativeTo(getOWLWorkspace());
-		dialog.setVisible(true);
-	}
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        OpenFromServerDialog dialog = new OpenFromServerDialog(getOWLEditorKit());
+        dialog.setLocationRelativeTo(getOWLWorkspace());
+        dialog.setVisible(true);
+    }
 }

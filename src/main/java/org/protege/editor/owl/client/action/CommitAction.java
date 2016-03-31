30cd107adccc83a572f9cd7cb5ca324540d8b4ec
johardi <josef.hardi@stanford.edu>
2016-03-31 14:21:14 -0700

    Use the new API to do change commits.

--- src/main/java/org/protege/editor/owl/client/action/CommitAction.java
+++ src/main/java/org/protege/editor/owl/client/action/CommitAction.java
@@ -1,27 +1,39 @@
 package org.protege.editor.owl.client.action;
 
 import org.protege.editor.core.ui.error.ErrorLogPanel;
-import org.protege.editor.owl.client.connect.ServerConnectionManager;
+import org.protege.editor.owl.client.ClientRegistry;
+import org.protege.editor.owl.client.api.Client;
+import org.protege.editor.owl.client.util.ChangeUtils;
 import org.protege.editor.owl.model.event.OWLModelManagerChangeEvent;
 import org.protege.editor.owl.model.event.OWLModelManagerListener;
 import org.protege.editor.owl.ui.UIHelper;
 import org.protege.editor.owl.ui.action.ProtegeOWLAction;
-import org.protege.owl.server.api.ChangeMetaData;
-import org.protege.owl.server.api.client.Client;
-import org.protege.owl.server.api.client.VersionedOntologyDocument;
-import org.protege.owl.server.api.exception.OWLServerException;
-import org.protege.owl.server.api.exception.UserDeclinedAuthenticationException;
-import org.protege.owl.server.util.ClientUtilities;
-import org.semanticweb.owlapi.model.OWLOntology;
+import org.protege.owl.server.changes.ChangeMetaData;
+import org.protege.owl.server.changes.api.VersionedOntologyDocument;
 
-import javax.swing.*;
-import java.awt.*;
+import org.semanticweb.owlapi.model.OWLOntology;
+import org.semanticweb.owlapi.model.OWLOntologyChange;
+
+import java.awt.Container;
+import java.awt.Frame;
 import java.awt.event.ActionEvent;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.SwingUtilities;
 
 public class CommitAction extends ProtegeOWLAction {
+
     private static final long serialVersionUID = 4601012273632698091L;
-    private ServerConnectionManager connectionManager;
-    private OWLModelManagerListener listener = new OWLModelManagerListener() {
+
+    private ExecutorService service = Executors.newSingleThreadExecutor();
+
+    private ClientRegistry clientRegistry;
+
+    private OWLModelManagerListener checkVersionOntology = new OWLModelManagerListener() {
         @Override
         public void handleChange(OWLModelManagerChangeEvent event) {
             updateEnabled();
@@ -30,14 +42,13 @@ public class CommitAction extends ProtegeOWLAction {
 
     @Override
     public void initialise() throws Exception {
-        updateEnabled();
-        getOWLModelManager().addListener(listener);
+        clientRegistry = ClientRegistry.getInstance(getOWLEditorKit());
+        getOWLModelManager().addListener(checkVersionOntology);
     }
 
     private void updateEnabled() {
-        connectionManager = ServerConnectionManager.get(getOWLEditorKit());
         OWLOntology ontology = getOWLEditorKit().getModelManager().getActiveOntology();
-        VersionedOntologyDocument vont = connectionManager.getVersionedOntology(ontology);
+        VersionedOntologyDocument vont = clientRegistry.getVersionedOntology(ontology);
         if (vont == null) {
             setEnabled(false);
         }
@@ -48,52 +59,44 @@ public class CommitAction extends ProtegeOWLAction {
 
     @Override
     public void dispose() throws Exception {
-        getOWLModelManager().removeListener(listener);
+        getOWLModelManager().removeListener(checkVersionOntology);
     }
 
     @Override
     public void actionPerformed(ActionEvent arg0) {
+        Container container = SwingUtilities.getAncestorOfClass(Frame.class, getOWLWorkspace());
         try {
-            Container owner = SwingUtilities.getAncestorOfClass(Frame.class, getOWLWorkspace());
-            final OWLOntology ontology = getOWLEditorKit().getModelManager().getActiveOntology();
-            final VersionedOntologyDocument vont = connectionManager.getVersionedOntology(ontology);
-            final String commitComment = JOptionPane.showInputDialog(owner, "Commit comment: ", "Commit",
-                    JOptionPane.PLAIN_MESSAGE);
-            if (vont == null) {
-                JOptionPane.showMessageDialog(owner,
-                        "Commit ignored because the ontology is not associated with a server");
-                return;
+            VersionedOntologyDocument vont = findActiveVersionedOntology();
+            String commitComment = JOptionPane.showInputDialog(container, "Commit comment: ", "Commit", JOptionPane.PLAIN_MESSAGE);
+            if (commitComment != null && !commitComment.isEmpty()) {
+                Client client = clientRegistry.getActiveClient();
+                service.submit(new DoCommit(client, vont, commitComment));
             }
-            if (commitComment == null) {
-                return; // user pressed cancel
-            }
-            Client client = connectionManager.createClient(ontology);
-
-            // TODO MetaData should not accept null commit comment..
-            connectionManager.getSingleThreadExecutorService()
-                    .submit(new DoCommit(client, vont, (!commitComment.isEmpty() ? commitComment : "")));
-        }
-        catch (UserDeclinedAuthenticationException udae) {
-            ; // ignore this because the user knows that he didn't want to
-              // authenticate
         }
         catch (Exception e) {
             handleError(e);
         }
-
     }
 
-    // ToDo modify the synchronization so that this can run in the background
-    // even while edits are in progress.
-    // make this part of the utility?
+    private VersionedOntologyDocument findActiveVersionedOntology() throws Exception {
+        OWLOntology ontology = getOWLEditorKit().getModelManager().getActiveOntology();
+        VersionedOntologyDocument vont = clientRegistry.getVersionedOntology(ontology);
+        if (vont == null) {
+            String template = "The ontology <%s> does not link to the server";
+            String message = String.format(template, ontology.getOntologyID().getOntologyIRI());
+            throw new Exception(message);
+        }
+        return vont;
+    }
+
     private class DoCommit implements Runnable {
         private Client client;
-        private VersionedOntologyDocument vont;
+        private VersionedOntologyDocument versionOntology;
         private String commitcomment;
 
         public DoCommit(Client client, VersionedOntologyDocument vont, String commitComment) {
             this.client = client;
-            this.vont = vont;
+            this.versionOntology = vont;
             this.commitcomment = commitComment;
         }
 
@@ -101,16 +104,15 @@ public class CommitAction extends ProtegeOWLAction {
         public void run() {
             ChangeMetaData metaData = new ChangeMetaData(commitcomment);
             try {
-                ClientUtilities.commit(client, metaData, vont);
+//                DocumentFactory factory = new DocumentFactoryImpl();
+//                RemoteOntologyDocument serverDoc = versionOntology.getServerDocument();
+//                OntologyDocumentRevision revision = versionOntology.getRevision();
+                List<OWLOntologyChange> uncommittedChanges = ChangeUtils.getUncommittedChanges(versionOntology);
+//                CommitBundle commits = new CommitBundles(uncommittedChanges);
+//                client.commit(project, commits);
             }
-            catch (OWLServerException ose) {
-                handleError(ose);
-            }
-            catch (Error e) {
+            catch (Exception e) {
                 handleError(e);
-            }
-            catch (RuntimeException re) {
-                handleError(re);
             }
         }
     }

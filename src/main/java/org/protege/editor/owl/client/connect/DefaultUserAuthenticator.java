7149fb1383a126bbc0b548d1c401059246ff5a25
johardi <josef.hardi@stanford.edu>
2016-03-31 13:49:49 -0700

    Change the implementation for hashing user's password

--- src/main/java/org/protege/editor/owl/client/connect/DefaultUserAuthenticator.java
+++ src/main/java/org/protege/editor/owl/client/connect/DefaultUserAuthenticator.java
@@ -1,16 +1,15 @@
 package org.protege.editor.owl.client.connect;
 
-import org.protege.owl.server.api.RmiLoginService;
-import org.protege.owl.server.security.DefaultLoginService;
+import org.protege.owl.server.connect.RmiLoginService;
 
 import edu.stanford.protege.metaproject.Manager;
 import edu.stanford.protege.metaproject.api.AuthToken;
+import edu.stanford.protege.metaproject.api.Factory;
 import edu.stanford.protege.metaproject.api.PlainPassword;
 import edu.stanford.protege.metaproject.api.Salt;
 import edu.stanford.protege.metaproject.api.SaltedPasswordDigest;
 import edu.stanford.protege.metaproject.api.UserAuthenticator;
 import edu.stanford.protege.metaproject.api.UserId;
-import edu.stanford.protege.metaproject.impl.UnauthorizedUserToken;
 
 /**
  * @author Josef Hardi <johardi@stanford.edu> <br>
@@ -26,12 +25,10 @@ public final class DefaultUserAuthenticator implements UserAuthenticator {
 
     @Override
     public AuthToken hasValidCredentials(UserId userId, PlainPassword password) throws Exception {
-        AuthToken authToken = new UnauthorizedUserToken(userId);
-        if (loginService instanceof DefaultLoginService) {
-            Salt userSalt = ((DefaultLoginService) loginService).getSalt(userId);
-            SaltedPasswordDigest passwordDigest = Manager.getFactory().createPasswordHasher().hash(password, userSalt);
-            authToken = loginService.login(userId, passwordDigest);
-        }
+        Salt userSalt = loginService.getSalt(userId);
+        Factory f = Manager.getFactory();
+        SaltedPasswordDigest passwordDigest = f.createPasswordHasher().hash(password, userSalt);
+        AuthToken authToken = loginService.login(userId, passwordDigest);
         return authToken;
     }
 }

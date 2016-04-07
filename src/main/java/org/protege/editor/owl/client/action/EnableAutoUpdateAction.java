e8c3a6e104415a3926292cc521ae98fddcb10e2f
johardi <josef.hardi@stanford.edu>
2016-04-07 15:23:48 -0700

    Update code for the auto-update action.

--- src/main/java/org/protege/editor/owl/client/action/EnableAutoUpdateAction.java
+++ src/main/java/org/protege/editor/owl/client/action/EnableAutoUpdateAction.java
@@ -1,38 +1,28 @@
 package org.protege.editor.owl.client.action;
 
 import org.protege.editor.core.ui.error.ErrorLogPanel;
-import org.protege.editor.owl.client.connect.ServerConnectionManager;
-import org.protege.editor.owl.ui.action.ProtegeOWLAction;
-import org.protege.owl.server.api.client.Client;
-import org.protege.owl.server.api.exception.UserDeclinedAuthenticationException;
-import org.protege.owl.server.changes.api.VersionedOntologyDocument;
-import org.protege.owl.server.util.ClientUtilities;
-import org.semanticweb.owlapi.model.OWLOntology;
 
-import javax.swing.*;
 import java.awt.event.ActionEvent;
-import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledFuture;
-import java.util.concurrent.TimeUnit;
 
-public class EnableAutoUpdateAction extends ProtegeOWLAction {
+import javax.swing.JCheckBoxMenuItem;
+import javax.swing.JMenuItem;
+
+public class EnableAutoUpdateAction extends AbstractClientAction {
+
     private static final long serialVersionUID = 1098490684799516207L;
+
     private ScheduledFuture<?> autoUpdate;
     private JCheckBoxMenuItem checkBoxMenuItem;
-    private ServerConnectionManager connectionManager;
-
-    public EnableAutoUpdateAction() {
-        // NO-OP
-    }
 
     @Override
     public void initialise() throws Exception {
-        connectionManager = ServerConnectionManager.get(getOWLEditorKit());
+        super.initialise();
     }
 
     @Override
     public void dispose() throws Exception {
-        // NO-OP
+        super.dispose();
     }
 
     public void setMenuItem(JMenuItem menu) {
@@ -41,14 +31,16 @@ public class EnableAutoUpdateAction extends ProtegeOWLAction {
 
     @Override
     public void actionPerformed(ActionEvent e) {
+        killAutoUpdate();
+        if (checkBoxMenuItem.isSelected()) {
+            autoUpdate = submit(new AutoUpdate(), 15); // TODO Make the auto-update timing adjustable
+        }
+    }
+
+    private void killAutoUpdate() {
         if (autoUpdate != null) {
             autoUpdate.cancel(false);
             autoUpdate = null;
-        }
-        else {
-            ScheduledExecutorService executor = connectionManager.getSingleThreadExecutorService();
-            // TODO: change this so that the sync delay value is fetched from the (new APIs) client configuration
-            autoUpdate = executor.scheduleWithFixedDelay(new AutoUpdate(), 15, 15, TimeUnit.SECONDS);
         }
     }
 
@@ -58,20 +50,15 @@ public class EnableAutoUpdateAction extends ProtegeOWLAction {
         @Override
         public void run() {
             try {
-                for (OWLOntology ontology : getOWLModelManager().getActiveOntologies()) {
-                    VersionedOntologyDocument vont = connectionManager.getVersionedOntology(ontology);
-                    if (vont != null) {
-                        Client client = connectionManager.createClient(vont.getServerDocument().getServerLocation());
-                        ClientUtilities.update(client, vont);
-                    }
+                if (getOntologyResource().isPresent()) {
+//                    getClient(). TODO: Implement update operation in the server
                 }
                 lastRunSuccessful = true;
             }
-            catch (UserDeclinedAuthenticationException udae) {
-                autoUpdate.cancel(false);
-                autoUpdate = null;
-                checkBoxMenuItem.setSelected(false);
-            }
+//            catch (UserDeclinedAuthenticationException udae) {
+//                killAutoUpdate();
+//                checkBoxMenuItem.setSelected(false);
+//            }
             catch (Throwable t) {
                 if (!lastRunSuccessful) {
                     ErrorLogPanel.showErrorDialog(t);

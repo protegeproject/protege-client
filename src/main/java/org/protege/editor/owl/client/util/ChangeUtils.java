48b4042d7ec6276fbf1bd115d1b73a4185ab8d4c
johardi <josef.hardi@stanford.edu>
2016-04-13 12:12:44 -0700

    Code refactor

--- src/main/java/org/protege/editor/owl/client/util/ChangeUtils.java
+++ src/main/java/org/protege/editor/owl/client/util/ChangeUtils.java
@@ -2,16 +2,15 @@ package org.protege.editor.owl.client.util;
 
 import org.protege.owl.server.api.exception.OWLServerException;
 import org.protege.owl.server.changes.OntologyDocumentRevision;
+import org.protege.owl.server.changes.ServerDocument;
 import org.protege.owl.server.changes.api.ChangeHistory;
 import org.protege.owl.server.changes.api.VersionedOntologyDocument;
-import org.protege.owl.server.connect.RemoteChangeService;
 import org.protege.owl.server.connect.RmiChangeService;
 import org.protege.owl.server.util.GetUncommittedChangesVisitor;
 
 import org.semanticweb.owlapi.model.OWLOntology;
 import org.semanticweb.owlapi.model.OWLOntologyChange;
 
-import java.io.File;
 import java.net.URI;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
@@ -23,65 +22,59 @@ import edu.stanford.protege.metaproject.api.Host;
 
 public class ChangeUtils {
 
-    private static RemoteChangeService getChangeService(Host remoteHost) throws OWLServerException {
+    private static RmiChangeService getChangeService(Host remoteHost) throws OWLServerException {
         URI remoteUri = remoteHost.getUri();
         String host = remoteUri.getHost();
         int port = remoteHost.getSecondaryPort().isPresent() ? remoteHost.getSecondaryPort().get().get() : remoteUri.getPort();
         try {
             Registry registry = LocateRegistry.getRegistry(host, port);
-            return (RemoteChangeService) registry.lookup(RmiChangeService.CHANGE_SERVICE);
+            return (RmiChangeService) registry.lookup(RmiChangeService.CHANGE_SERVICE);
         }
         catch (RemoteException | NotBoundException e) {
             throw new OWLServerException(e);
         }
     }
 
-    public static List<OWLOntologyChange> getUncommittedChanges(VersionedOntologyDocument versionedOntology)
-            throws OWLServerException {
-        try {
-            ChangeHistory remoteChanges = getLatestChanges(versionedOntology);
-            if (!remoteChanges.isEmpty()) {
-                versionedOntology.appendLocalHistory(remoteChanges);
-            }
-            
-            final OntologyDocumentRevision startRevision = OntologyDocumentRevision.START_REVISION;
-            final OntologyDocumentRevision endRevision = remoteChanges.getEndRevision();
-            ChangeHistory uncommittedChanges = versionedOntology.getLocalHistory().cropChanges(startRevision, endRevision);
-            
-            final OWLOntology ontology = versionedOntology.getOntology();
-            List<OWLOntologyChange> baselineHistory = uncommittedChanges.getChanges(ontology);
-            GetUncommittedChangesVisitor visitor = new GetUncommittedChangesVisitor(ontology);
-            for (OWLOntologyChange change : baselineHistory) {
-                change.accept(visitor);
-            }
-            return visitor.getChanges();
+    public static List<OWLOntologyChange> getUncommittedChanges(VersionedOntologyDocument versionedOntology) throws OWLServerException {
+        ChangeHistory remoteChanges = getLatestChanges(versionedOntology);
+        if (!remoteChanges.isEmpty()) {
+            versionedOntology.appendChangeHistory(remoteChanges);
         }
-        catch (Exception e) {
-            throw new OWLServerException(e);
+
+        final OntologyDocumentRevision startRevision = OntologyDocumentRevision.START_REVISION;
+        final OntologyDocumentRevision endRevision = remoteChanges.getEndRevision();
+        ChangeHistory uncommittedChanges = versionedOntology.getLocalHistory().cropChanges(startRevision, endRevision);
+
+        final OWLOntology ontology = versionedOntology.getOntology();
+        List<OWLOntologyChange> baselineHistory = uncommittedChanges.getChanges(ontology);
+        GetUncommittedChangesVisitor visitor = new GetUncommittedChangesVisitor(ontology);
+        for (OWLOntologyChange change : baselineHistory) {
+            change.accept(visitor);
         }
+        return visitor.getChanges();
     }
 
     public static ChangeHistory getAllChanges(VersionedOntologyDocument versionedOntology) throws OWLServerException {
+        ServerDocument serverDocument = versionedOntology.getServerDocument();
+        RmiChangeService changeService = getChangeService(serverDocument.getHost());
         try {
-            final RemoteChangeService changeService = getChangeService(versionedOntology.getRemoteHost());
-            File remoteFile = versionedOntology.getRemoteFile();
-            ChangeHistory allChanges = changeService.getAllChanges(remoteFile);
+            ChangeHistory allChanges = changeService.getAllChanges(serverDocument);
             return allChanges;
         }
-        catch (Exception e) {
+        catch (RemoteException e) {
             throw new OWLServerException(e);
         }
     }
 
     public static ChangeHistory getLatestChanges(VersionedOntologyDocument versionedOntology) throws OWLServerException {
+        ServerDocument serverDocument = versionedOntology.getServerDocument();
+        OntologyDocumentRevision localHeadRevision = versionedOntology.getLocalHistory().getEndRevision();
+        RmiChangeService changeService = getChangeService(serverDocument.getHost());
         try {
-            final RemoteChangeService changeService = getChangeService(versionedOntology.getRemoteHost());
-            File remoteFile = versionedOntology.getRemoteFile();
-            OntologyDocumentRevision localHeadRevision = versionedOntology.getLocalHistory().getEndRevision();
-            ChangeHistory latestChanges = changeService.getLatestChanges(remoteFile, localHeadRevision);
+            ChangeHistory latestChanges = changeService.getLatestChanges(serverDocument, localHeadRevision);
             return latestChanges;
         }
-        catch (Exception e) {
+        catch (RemoteException e) {
             throw new OWLServerException(e);
         }
     }

7e68a05cac29337f2b9887948237e62f0bd41d17
johardi <josef.hardi@stanford.edu>
2016-04-13 12:14:11 -0700

    Use Project instance instead of RemoteServerDocument to display project info.

--- src/main/java/org/protege/editor/owl/client/panel/ServerTableModel.java
+++ src/main/java/org/protege/editor/owl/client/panel/ServerTableModel.java
@@ -2,49 +2,29 @@ package org.protege.editor.owl.client.panel;
 
 import org.protege.editor.owl.client.api.Client;
 import org.protege.owl.server.api.exception.OWLServerException;
-import org.protege.owl.server.changes.api.RemoteServerDirectory;
-import org.protege.owl.server.changes.api.RemoteServerDocument;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Comparator;
-import java.util.Iterator;
 import java.util.List;
 
 import javax.swing.table.AbstractTableModel;
+
+import edu.stanford.protege.metaproject.api.Project;
+import edu.stanford.protege.metaproject.api.ProjectId;
 
 public class ServerTableModel extends AbstractTableModel {
 
     private static final long serialVersionUID = -1677982790864801841L;
 
     public enum Column {
-        SERVER_DOCUMENT_LOCATION;
+        REMOTE_PROJECT;
     }
 
-    private List<RemoteServerDocument> serverDocuments = new ArrayList<RemoteServerDocument>();
+    private List<Project> remoteProjects;
 
-    public void loadServerData(Client client, RemoteServerDirectory dir) throws OWLServerException {
-        List<RemoteServerDocument> docs = new ArrayList<RemoteServerDocument>(client.list(dir));
-
-        /*
-         * Tim and Jenn decided that for the first release, there shouldn't be
-         * any server directories exposed to the user - only a list of ontology
-         * documents. Server folder structure will come later.
-         */
-        Iterator<RemoteServerDocument> iterator = docs.iterator();
-        while (iterator.hasNext()) {
-            RemoteServerDocument doc = iterator.next();
-            if (doc instanceof RemoteServerDirectory) {
-                iterator.remove();
-            }
-        }
-        Collections.sort(docs, new Comparator<RemoteServerDocument>() {
-            @Override
-            public int compare(RemoteServerDocument doc1, RemoteServerDocument doc2) {
-                return doc1.getServerLocation().compareTo(doc2.getServerLocation());
-            }
-        });
-        serverDocuments = docs;
+    public void initialize(Client client) throws OWLServerException {
+        remoteProjects = new ArrayList<>(client.getProjects());
+        Collections.sort(remoteProjects);
         fireTableStructureChanged();
     }
 
@@ -57,8 +37,8 @@ public class ServerTableModel extends AbstractTableModel {
     public String getColumnName(int col) {
         Column column = Column.values()[col];
         switch (column) {
-        case SERVER_DOCUMENT_LOCATION:
-            return "Ontology Documents";
+        case REMOTE_PROJECT:
+            return "Available projects";
         default:
             throw new IllegalStateException("Programmer missed a case");
         }
@@ -66,32 +46,16 @@ public class ServerTableModel extends AbstractTableModel {
 
     @Override
     public int getRowCount() {
-        return serverDocuments.size();
+        return remoteProjects.size();
     }
 
     @Override
     public Object getValueAt(int row, int col) {
-        Column column = Column.values()[col];
-        RemoteServerDocument doc = serverDocuments.get(row);
-        switch (column) {
-        case SERVER_DOCUMENT_LOCATION:
-            String fragment = doc.getServerLocation().getFragment();
-            if (fragment == null || fragment.isEmpty()) {
-                String path = doc.getServerLocation().toURI().getPath();
-                if (path.endsWith("/")) {
-                    path = path.substring(0, path.length() - 1);
-                }
-                int start = path.lastIndexOf('/');
-                return path.substring(start + 1);
-            }
-            else
-                return fragment;
-        default:
-            throw new IllegalStateException("Programmer missed a case");
-        }
+        Project project = remoteProjects.get(row);
+        return project.getId();
     }
 
-    public RemoteServerDocument getValueAt(int row) {
-        return serverDocuments.get(row);
+    public ProjectId getValueAt(int row) {
+        return (ProjectId) getValueAt(row, -1);
     }
 }

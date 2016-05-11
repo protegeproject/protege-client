a5c24bcd27660c66406965aa7378200af7f0c486
johardi <josef.hardi@stanford.edu>
2016-05-11 14:15:23 -0700

    Update code due to protege-server changed

--- src/main/java/org/protege/editor/owl/client/ui/HistoryTableModel.java
+++ src/main/java/org/protege/editor/owl/client/ui/HistoryTableModel.java
@@ -59,7 +59,7 @@ public class HistoryTableModel extends AbstractTableModel {
 
     @Override
     public int getRowCount() {
-        return DocumentRevision.delta(changes.getStartRevision(), changes.getEndRevision());
+        return DocumentRevision.distance(changes.getStartRevision(), changes.getEndRevision());
     }
 
     @Override

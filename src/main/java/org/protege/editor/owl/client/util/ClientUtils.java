a9a2e340c2062a35691174838cdee345b2230ca3
johardi <josef.hardi@stanford.edu>
2016-05-26 17:53:54 -0700

    Building an ontology or versioned ontology will require OWLOntologyManager

--- src/main/java/org/protege/editor/owl/client/util/ClientUtils.java
+++ src/main/java/org/protege/editor/owl/client/util/ClientUtils.java
@@ -13,11 +13,9 @@ import org.protege.editor.owl.server.versioning.api.RevisionMetadata;
 import org.protege.editor.owl.server.versioning.api.ServerDocument;
 import org.protege.editor.owl.server.versioning.api.VersionedOWLOntology;
 
-import org.semanticweb.owlapi.apibinding.OWLManager;
 import org.semanticweb.owlapi.model.AddImport;
 import org.semanticweb.owlapi.model.MissingImportHandlingStrategy;
 import org.semanticweb.owlapi.model.OWLImportsDeclaration;
-import org.semanticweb.owlapi.model.OWLMutableOntology;
 import org.semanticweb.owlapi.model.OWLOntology;
 import org.semanticweb.owlapi.model.OWLOntologyChange;
 import org.semanticweb.owlapi.model.OWLOntologyCreationException;
@@ -97,39 +95,34 @@ public class ClientUtils {
      * @throws ClientRequestException
      * @throws OWLOntologyCreationException
      */
-    public static OWLOntology buildOntology(ServerDocument serverDocument)
+    public static OWLOntology buildOntology(ServerDocument serverDocument, OWLOntologyManager owlManager)
             throws ClientRequestException, OWLOntologyCreationException {
         ChangeHistory remoteChangeHistory = ChangeUtils.getAllChanges(serverDocument);
-        OWLMutableOntology targetOntology = createEmptyMutableOntology();
-        updateOntology(targetOntology, remoteChangeHistory);
+        OWLOntology targetOntology = owlManager.createOntology();
+        updateOntology(targetOntology, remoteChangeHistory, owlManager);
         return targetOntology;
     }
 
     /**
-     * Update an existing <code>currentOntology</code> with recent changes from the remote change
+     * Update an existing <code>targetOntology</code> with recent changes from the remote change
      * history specified by the last local revision <code>localHead</code> and the
-     * <code>serverDocument</code> reference. The method will return a new copy rather than
-     * overriding the input <code>currentOntology</code>.
+     * <code>serverDocument</code> reference.
      *
      * @param serverDocument
      *          The input server document.
      * @param localHead
      *          The local HEAD reference.
-     * @param currentOntology
+     * @param targetOntology
      *          The target ontology to be updated
      * @return An updated OWL ontology instance.
      * @throws ClientRequestException
      * @throws OWLOntologyCreationException
      */
     public static OWLOntology updateOntology(ServerDocument serverDocument, DocumentRevision localHead,
-            final OWLOntology currentOntology) throws ClientRequestException, OWLOntologyCreationException {
+            final OWLOntology targetOntology) throws ClientRequestException, OWLOntologyCreationException {
         ChangeHistory remoteChangeHistory = ChangeUtils.getLatestChanges(serverDocument, localHead);
-        if (currentOntology instanceof OWLMutableOntology) {
-            OWLMutableOntology targetOntology = (OWLMutableOntology) currentOntology;
-            updateOntology(targetOntology, remoteChangeHistory);
-            return targetOntology;
-        }
-        throw new RuntimeException("Unable to update the ontology. The input ontology is immutable");
+        updateOntology(targetOntology, remoteChangeHistory, targetOntology.getOWLOntologyManager());
+        return targetOntology;
     }
 
     /**
@@ -142,11 +135,11 @@ public class ClientUtils {
      * @throws ClientRequestException
      * @throws OWLOntologyCreationException
      */
-    public static VersionedOWLOntology buildVersionedOntology(ServerDocument serverDocument)
+    public static VersionedOWLOntology buildVersionedOntology(ServerDocument serverDocument, OWLOntologyManager owlManager)
             throws ClientRequestException, OWLOntologyCreationException {
         ChangeHistory remoteChangeHistory = ChangeUtils.getAllChanges(serverDocument);
-        OWLMutableOntology targetOntology = createEmptyMutableOntology();
-        updateOntology(targetOntology, remoteChangeHistory);
+        OWLOntology targetOntology = owlManager.createOntology();
+        updateOntology(targetOntology, remoteChangeHistory, owlManager);
         return new VersionedOWLOntologyImpl(serverDocument, targetOntology, remoteChangeHistory);
     }
 
@@ -159,37 +152,29 @@ public class ClientUtils {
      *            The input server document.
      * @param localHead
      *            The local HEAD reference.
-     * @param currentOntology
+     * @param targetOntology
      *            The target ontology that will be updated
      * @return A new version ontology instance
      * @throws ClientRequestException
      * @throws OWLOntologyCreationException
      */
     public static VersionedOWLOntology buildVersionedOntology(ServerDocument serverDocument, DocumentRevision localHead,
-            ChangeHistory localHistory, OWLOntology currentOntology)
+            ChangeHistory localHistory, final OWLOntology targetOntology)
                     throws ClientRequestException, OWLOntologyCreationException {
         ChangeHistory remoteChangeHistory = ChangeUtils.getLatestChanges(serverDocument, localHead);
-        if (currentOntology instanceof OWLMutableOntology) {
-            OWLMutableOntology targetOntology = (OWLMutableOntology) currentOntology;
-            updateOntology(targetOntology, remoteChangeHistory);
-            updateChangeHistory(localHistory, remoteChangeHistory);
-            return new VersionedOWLOntologyImpl(serverDocument, targetOntology, localHistory);
-        }
-        throw new RuntimeException("Unable to update the ontology. The input ontology is immutable");
+        updateOntology(targetOntology, remoteChangeHistory, targetOntology.getOWLOntologyManager());
+        updateChangeHistory(localHistory, remoteChangeHistory);
+        return new VersionedOWLOntologyImpl(serverDocument, targetOntology, localHistory);
     }
 
     /*
      * Private utility methods
      */
 
-    private static OWLMutableOntology createEmptyMutableOntology() throws OWLOntologyCreationException {
-        return (OWLMutableOntology) OWLManager.createOWLOntologyManager().createOntology();
-    }
-
-    private static void updateOntology(OWLMutableOntology placeholder, ChangeHistory changeHistory) {
+    private static void updateOntology(OWLOntology placeholder, ChangeHistory changeHistory, OWLOntologyManager manager) {
         List<OWLOntologyChange> changes = ChangeHistoryUtils.getOntologyChanges(changeHistory, placeholder);
-        placeholder.applyChanges(changes);
-        fixMissingImports(placeholder, changes);
+        manager.applyChanges(changes);
+        fixMissingImports(placeholder, changes, manager);
     }
 
     private static void updateChangeHistory(ChangeHistory changeHistory, ChangeHistory incomingChangeHistory) {
@@ -202,24 +187,23 @@ public class ClientUtils {
         }
     }
 
-    private static void fixMissingImports(OWLMutableOntology targetOntology, List<OWLOntologyChange> changes) {
-        OWLOntologyManager owlManager = targetOntology.getOWLOntologyManager();
+    private static void fixMissingImports(OWLOntology ontology, List<OWLOntologyChange> changes, OWLOntologyManager manager) {
         OWLOntologyLoaderConfiguration configuration = new OWLOntologyLoaderConfiguration();
         configuration = configuration.setMissingImportHandlingStrategy(MissingImportHandlingStrategy.SILENT);
         configuration = configuration.setMissingOntologyHeaderStrategy(MissingOntologyHeaderStrategy.IMPORT_GRAPH);
         
-        final Set<OWLImportsDeclaration> declaredImports = targetOntology.getImportsDeclarations();
+        final Set<OWLImportsDeclaration> declaredImports = ontology.getImportsDeclarations();
         Set<OWLImportsDeclaration> missingImports = new TreeSet<OWLImportsDeclaration>();
         for (OWLOntologyChange change : changes) {
             if (change instanceof AddImport) {
                 OWLImportsDeclaration importDecl = ((AddImport) change).getImportDeclaration();
-                if (declaredImports.contains(importDecl) && owlManager.getImportedOntology(importDecl) == null) {
+                if (declaredImports.contains(importDecl) && manager.getImportedOntology(importDecl) == null) {
                     missingImports.add(importDecl);
                 }
             }
         }
         for (OWLImportsDeclaration importDecl : missingImports) {
-            owlManager.makeLoadImportRequest(importDecl, configuration);
+            manager.makeLoadImportRequest(importDecl, configuration);
         }
     }
 }

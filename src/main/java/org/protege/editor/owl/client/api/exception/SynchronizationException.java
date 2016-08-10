108e79aae9e6b7f919c778f51bc9764220464c91
johardi <josef.hardi@stanford.edu>
2016-08-09 17:09:28 -0700

    Make SynchronizationException part of ClientRequestException

--- src/main/java/org/protege/editor/owl/client/api/exception/SynchronizationException.java
+++ src/main/java/org/protege/editor/owl/client/api/exception/SynchronizationException.java
@@ -4,7 +4,7 @@ package org.protege.editor.owl.client.api.exception;
  * @author Josef Hardi <johardi@stanford.edu> <br>
  * Stanford Center for Biomedical Informatics Research
  */
-public class SynchronizationException extends OWLClientException {
+public class SynchronizationException extends ClientRequestException {
 
     private static final long serialVersionUID = 1145694593110048161L;
 

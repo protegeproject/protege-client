7ca0fd09b4691cf715ea6320a564e0e36784373e
Rafael Goncalves <goncalvjr@gmail.com>
2016-09-14 11:07:09 -0700

    Fixes #69 and #66
    Proper policy checks shouldnt be bypassed

--- src/main/java/org/protege/editor/owl/client/LocalHttpClient.java
+++ src/main/java/org/protege/editor/owl/client/LocalHttpClient.java
@@ -1161,16 +1161,10 @@ public class LocalHttpClient implements Client, ClientSessionListener {
 	 * Utility methods
 	 */
 	public boolean queryProjectPolicy(UserId userId, ProjectId projectId, OperationId operationId) {
-		if (getClientType() == UserType.ADMIN) {
-			return false;
-		}
 		return serverConfiguration.isOperationAllowed(operationId, projectId, userId);
 	}
 
 	private boolean queryAdminPolicy(UserId userId, OperationId operationId) {
-		if (getClientType() == UserType.NON_ADMIN) {
-			return false;
-		}
 		return serverConfiguration.isOperationAllowed(operationId, userId);
 	}
 

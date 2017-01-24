0259422a6aaf410b8af42ee1328b6fad7eeb83d3
Bob Dionne <dionne@dionne-associates.com>
2017-01-24 15:04:48 -0500

    Support for soem inferred views

--- src/main/java/org/protege/editor/owl/client/TabViewableChecker.java
+++ src/main/java/org/protege/editor/owl/client/TabViewableChecker.java
@@ -36,6 +36,8 @@ public class TabViewableChecker implements TabViewable {
 		wf_man_tabs.add("Active Ontology");
 		wf_man_tabs.add("Object Properties");
 		wf_man_tabs.add("Classification results");
+		wf_man_tabs.add("Superclass hierarchy (inferred)");
+		wf_man_tabs.add("Superclass hierarchy");
 		
 		// currently modelers can see all the same tabs, but only in read only mode
 		
